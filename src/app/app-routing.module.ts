d6407dbd5432421a5a4751258ab46bf8181ada51
Njoki Kamau <njokifaith61@gmail.com>
2020-06-28 15:51:25 +0300

    git add-commit -m add 404 informative page and redirect path in routing module to improve user experience

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,16 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 //routes tells views what to display
+//the Notfound component is a more formal way to infor user of an error
 //import all components, add paths to them in in const routes, and add router links in the html
 const routes: Routes = [
   {path:"profile", component: UserComponent},
   {path:"portfolio", component: RepoComponent},
   {path:"navbar", component: NavbarComponent},
   {path:"searchForm", component: SearchFormComponent},
-  {path:"notFound", component: NotFoundComponent},
-
+  {path:"**", component: NotFoundComponent},
+  //the astericks are used as sort of a wild card in case of rout not present
+//important to add a redirect path in case of an empty path in the app., to avoid empty screens
   { path: '', redirectTo:"profile", pathMatch:"full"},
 ];
 
